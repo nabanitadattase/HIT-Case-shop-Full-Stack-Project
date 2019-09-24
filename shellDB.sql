8aed43528a8a5fc740cc9feda6cb97151800cb9c
nabanitadattase <47370928+nabanitadattase@users.noreply.github.com>
2019-09-25 00:06:43 +0200

    Hit Case Shop Application

---
+++ shellDB.sql
@@ -0,0 +1,18 @@
+CREATE TABLE SHELL (
+        MODEL VARCHAR(8) NOT NULL,
+        BRAND  VARCHAR(15) NOT NULL,
+	MANUFACTURER VARCHAR(15) NOT NULL,
+        COLOR VARCHAR(10) NOT NULL,        
+        PRIMARY KEY (MODEL)
+);
+
+
+
+INSERT INTO SHELL VALUES ('J7', 'Nokia', 'Finland', 'Red');
+INSERT INTO SHELL  VALUES ('S2', 'Samsung', 'Vietnam', 'Blue');
+INSERT INTO SHELL  VALUES ('G8', 'Apple', 'China', 'White');
+
+
+
+
+
