82891f2fc17a0925e152c3fc09a33cb380eab116
nabanitadattase <47370928+nabanitadattase@users.noreply.github.com>
2019-09-25 00:13:10 +0200

    Hit Case Shop Application

---
+++ MobileShell/build/generated-sources/ap-source-output/com/shellentity/Shell_.java
@@ -0,0 +1,16 @@
+package com.shellentity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2019-09-24T00:18:26")
+@StaticMetamodel(Shell.class)
+public class Shell_ { 
+
+    public static volatile SingularAttribute<Shell, String> model;
+    public static volatile SingularAttribute<Shell, String> color;
+    public static volatile SingularAttribute<Shell, String> manufacturer;
+    public static volatile SingularAttribute<Shell, String> brand;
+
+}
